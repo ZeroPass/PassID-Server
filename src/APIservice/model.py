0b6aa18a8ebb90e6715ec0830ca9472732cfd97e
Unknown <nejko90@gmail.com>
2019-09-24 09:07:06 +0200

    json rpc added

---
+++ src/APIservice/model.py
@@ -0,0 +1,10 @@
+'''
+    File name: model.py
+    Author: ZeroPass - Nejc Skerjanc
+    License: MIT lincense
+    Python Version: 3.6
+'''
+
+
+class Model:
+    """Set of functions to use it in the API (JSON-RPC)"""
