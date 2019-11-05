2391e7ac5d9e641db1f708d81f98d53dd96ee109
Unknown <nejko90@gmail.com>
2019-11-05 21:34:51 +0100

    few changes

--- src/management/main.py
+++ src/management/main.py
@@ -8,8 +8,8 @@
 from management.builder import Builder
 
 
-DSC_CRL = open('C://Users/nejko/Desktop/ZeroPass/B1/random/parseCSCAandCRL/data/icaopkd-001-dsccrl-003749.ldif', 'rb')
-CSCA = open('C://Users/nejko/Desktop/ZeroPass/B1/random/parseCSCAandCRL/data/icaopkd-002-ml-000123.ldif', 'rb')
+DSC_CRL = open('C://Users/nejko/Desktop/ZeroPass/B1/random/parseCSCAandCRL/data/abc/icaopkd-001-dsccrl-003903.ldif', 'rb')
+CSCA = open('C://Users/nejko/Desktop/ZeroPass/B1/random/parseCSCAandCRL/data/abc/icaopkd-002-ml-000131.ldif', 'rb')
 
 parse = Builder(CSCA, DSC_CRL)
 re = 9
