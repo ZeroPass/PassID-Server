ea83e113936937e3af85623c65cc01b66f40fcfd
smlu <smlu@s5.net>
2019-11-07 15:30:55 +0100

    Add new command line options

--- src/APIservice/unittest/test_server.py
+++ src/APIservice/unittest/test_server.py
@@ -14,51 +14,39 @@ from pymrtd import ef
 
 
 class DevProto(proto.PassIdProto):
+    def __init__(self, storage: proto.StorageAPI, cttl: int, fc: bool, no_tcv: bool):
+        super().__init__(storage, cttl)
+        self._fc = fc
+        self._no_tcv = no_tcv
+
     def createNewChallenge(self) -> proto.Challenge:
-        now = datetime.utcnow()
-        c = proto.Challenge.fromhex("47E4EE7F211F73265DD17658F6E21C1318BD6C81F37598E20A2756299542EFCF")
-        self._db.addChallenge(c, now)
-        return c
+        if self._fc:
+            now = datetime.utcnow()
+            c = proto.Challenge.fromhex("47E4EE7F211F73265DD17658F6E21C1318BD6C81F37598E20A2756299542EFCF")
+            self._db.addChallenge(c, now)
+            return c
+        return super().createNewChallenge()
 
     def _get_account_expiration(self, uid: proto.UserId):
         return datetime.utcnow() + timedelta(minutes=1)
 
     def validateCertificatePath(self, sod: ef.SOD):
-            pass
+            if not self._no_tcv:
+                super().validateCertificatePath(sod)
     
 class DevApiServer(PassIdApiServer):
-    def __init__(self, db: proto.StorageAPI, config: Config):
+    def __init__(self, db: proto.StorageAPI, config: Config, fc=False, no_tcv=False):
         super().__init__(db, config)
-        self._proto = DevProto(db, config.challenge_ttl)
+        self._proto = DevProto(db, config.challenge_ttl, fc, no_tcv)
 
 
 def main():
-
     ap = argparse.ArgumentParser()
-
-    ap.add_argument("-u", "--url", default='0.0.0.0',
-        type=str, help="server http address")
-
-    ap.add_argument("-p", "--port",
-        type=int, help="server listening port")
-
-    ap.add_argument("-dev", default=False,
-        action='store_true', help="start development version of server. This will use pre-set fixed challenge instead of random generated")
-
     ap.add_argument("--challenge-ttl", default=300,
         type=int, help="number of seconds until requested challenge expires")
 
     ap.add_argument("-c", "--cert", default=str(_script_path / "tls/passid_server.cer"),
         type=str, help="server TLS certificate")
-
-    ap.add_argument("-k", "--key", default=str(_script_path / "tls/server_key.pem"),
-        type=str, help="server TLS private key")
-
-    ap.add_argument("-no-tls", default=False,
-        action='store_true', help="do not use secure TLS connection")
-
-    ap.add_argument("-m", "--mdb", default=False,
-        type=str, help="use MemoryDB for database")
 
     ap.add_argument("--db-user", default="",
         type=str, help="database user name")
@@ -68,6 +56,31 @@ def main():
 
     ap.add_argument("--db-name", default="",
         type=str, help="database name")
+
+    ap.add_argument("-dev", default=False,
+        action='store_true', help="start development version of server")
+
+    ap.add_argument("-dev-fc", default=False,
+        action='store_true', help="dev option: use pre-set fixed challenge instead of random generated")
+
+    ap.add_argument("-dev-no-tcv", default=False,
+        action='store_true', help="dev option: do not verify eMRTD PKI trust-chain")
+
+    ap.add_argument("-k", "--key", default=str(_script_path / "tls/server_key.pem"),
+        type=str, help="server TLS private key")
+
+    ap.add_argument("-m", "--mdb", default=False,
+        type=str, help="use MemoryDB for database. --db-* args will be ignored")
+
+    ap.add_argument("-no-tls", default=False,
+        action='store_true', help="do not use secure TLS connection")
+
+    ap.add_argument("-p", "--port",
+        type=int, help="server listening port")
+
+    ap.add_argument("-u", "--url", default='0.0.0.0',
+        type=str, help="server http address")
+
 
     args = vars(ap.parse_args())
     if args['port'] is None:
@@ -98,11 +111,12 @@ def main():
     else:
         db = proto.DatabaseAPI(config.database.user, config.database.pwd, config.database.db)
 
+
+    # Setup and run server
     if args["dev"]:
-        sapi = DevApiServer(db, config)
+        sapi = DevApiServer(db, config, args['dev_fc'], args['dev_no_tcv'])
     else:
         sapi = PassIdApiServer(db, config)
-
     sapi.start()
 
 if __name__ == "__main__":
