9ee67d6d5ddfdada29740e9fde00ecd2236a8613
smlu <smlu@s5.net>
2019-12-13 17:28:41 +0100

    Update (make_cert.sh): Add DER encoded certificate

--- src/APIservice/apiserver/tls/make_cert.sh
+++ src/APIservice/apiserver/tls/make_cert.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 openssl req -new -x509 -nodes -days 356 -sha384 -subj "/O=ZeroPass PassID/OU=PassID Server/CN=PassID Server" -key server_key.pem -out "passid_server_new.cer"
+
+# der encoding
+openssl x509 -outform der -in "passid_server_new.cer" -out "passid_server_new.der"
